de4757d113b4da3e7d4497838f7c6480bd3231e8
= <ibrahim@mercadolar.com>
2017-06-05 19:35:42 -0400

    yml

---
+++ Carupano.Azure/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Carupano.Configuration
+{
+    public static class Extensions
+    {
+        public static BoundedContextModelBuilder WithAzureServiceBus(this BoundedContextModelBuilder model, string connectionString)
+        {
+            model.Services(cfg =>
+            {
+                Func<IServiceProvider, Azure.AzureServiceBus> factory = (svcs) =>
+                {
+                    //TODO: endpoint name
+                    return new Azure.AzureServiceBus(connectionString, String.Empty, svcs.GetService<ISerialization>(), svcs.GetService<RouteTable>());
+                };
+                cfg.AddScoped<ICommandBus>(factory);
+                cfg.AddScoped<IEventBus>(factory);
+            });
+            return model;
+        }
+    }
+}
